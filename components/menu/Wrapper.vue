29d29067955bceab474b876243dae87d37b102ad
YC <yctan7@gmail.com>
2020-07-05 13:33:47 +0800

    Round3

--- components/menu/Wrapper.vue
+++ components/menu/Wrapper.vue
@@ -11,8 +11,57 @@
         shadow-2xl
         pt-16
         ">
+        <menuLink
+            :url="'/dashboard'"
+            :class="{
+                'text-gray-100':!$route.path.includes('/dashboard'),
+                'text-white':$route.path.includes('/dashboard'),
+                'hover:bg-gray-600':true,
+                'hover:text-white':true }">
+            <template v-slot:icon-word>
+                <div class="flex items-center justify-start overflow-hidden">
+                <i class="fas fa-home ml-6 mr-4"></i>
+                    <label class="flex-shrink-0 ml-2 text-sm font-sans tracking-wide cursor-pointer">Dashboard</label>
+                </div>
+            </template>
+        </menuLink>
+
+        <menuLinkList :url="'/products'"
+                :class="{
+                'text-gray-500':!$route.path.includes('/products'),
+                'text-white':$route.path.includes('/products'),
+                'cursor-pointer':true
+        }">
+            <template v-slot:icon-word>
+                <div class="flex items-center justify-start overflowx-hidden hover:text-white">
+                    <i class="fas fa-truck-loading ml-6 mr-4"></i>
+                    <label class="flex-shrink-0 ml-2 text-sm font-sans tracking-wide cursor-pointer">Products</label>
+                </div>
+             </template>
+
+            <template v-slot:sub-link>
+                <menuLink
+                    :url="'/products/new'"
+                    :class="{
+                    'text-gray-500':!$route.path.includes('/products/new'),
+                    'textwhite':$route.path.includes('/products/new'),
+                    'hover:text-white':true,
+                    'cursor-pointer':true
+                    }">
+                <template v-slot:icon-word>
+                    <div class="flex items-center justify-start
+                    overflow-hidden py-2">
+                    <i class="fas fa-coffee ml-6 mr-4"></i>
+                    <label class="flex-shrink-0 ml-2 text-sm
+                    font-sans tracking-wide cursor-pointer">New Product</label>
+                    </div>
+                </template>
+                </menuLink>
+            </template>
+        </menuLinkList>
     </div>
 </template>
+
 <script>
     import MenuLink from "~/components/items/MenuLink.vue";
     import MenuLinkList from "~/components/items/MenuLinkList.vue";
--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -3,6 +3,7 @@
         <naviWrapper class="absolute top-0 z-30 w-screen"/>
         <div class="flex-1">
             <div class="flex">
+                <menuWrapper class="z-20"/>
                 <nuxt class="pt-20 bg-gray-200 w-full h-screen pb-5
 px-5 overflow-y-auto" />
             </div>
@@ -16,7 +17,8 @@ px-5 overflow-y-auto" />
     export default {
         name:"dashboard",
         components:{
-            NaviWrapper
+            NaviWrapper,
+            MenuWrapper
         }
     }
 </script>
